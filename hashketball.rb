6d9b35ac0d59d9a09856c14d0d43fb5f8839cb2f
Tomas Gonzalez <tomas@rtomasgonzalez.com>
2020-07-04 02:58:42 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -59,8 +59,8 @@ def game_hash
   away: {
     team_name: "Charlotte Hornets"
     colors: ["Turquoise", "Purple"]
-    players: [
-      "Jeff Adrien", => {
+    players: {
+      "Jeff Adrien" => {
       number: 4,
       shoe: 18,
       points: 10,
@@ -70,7 +70,7 @@ def game_hash
       blocks: 7,
       slam_dunks: 2,
     },
-      "RBismack Biyombo", => {
+      "RBismack Biyombo" => {
       number: 0,
       shoe: 16,
       points: 12,
@@ -80,7 +80,7 @@ def game_hash
       blocks: 15,
       slam_dunks: 10,
     },
-      "DeSagna Diop", => {
+      "DeSagna Diop" => {
       number: 2,
       shoe: 14,
       points: 24,
@@ -90,7 +90,7 @@ def game_hash
       blocks: 5,
       slam_dunks: 5,
     },
-      "Ben Gordon", => {
+      "Ben Gordon" => {
       number: 8,
       shoe: 15,
       points: 33,
@@ -100,7 +100,7 @@ def game_hash
       blocks: 1,
       slam_dunks: 0,
     }
-      "Kemnba Walker", => {
+      "Kemnba Walker" => {
       number: 33,
       shoe: 15,
       points: 6,
@@ -110,5 +110,5 @@ def game_hash
       blocks: 5,
       slam_dunks: 12,
     }
-    ]
+    }
   }
