d6a84ac0e5f86ce6acd7b0c17635c319f090a7a6
Tomas Gonzalez <tomas@rtomasgonzalez.com>
2020-07-04 02:36:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,11 @@
 # Write your code here!
 def game_hash
-  home = {
-    :team_name
-    :colors
-    :players [{}]
+  { home: {
+    team_name:
+    colors:
+    players: 
   }
-  away = {
+  away: {
     :team_name
     :colors
     :players
