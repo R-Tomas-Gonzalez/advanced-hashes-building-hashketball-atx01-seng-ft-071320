696f47715ad4d104f2b69b8f25e3d6794dfc08b8
Tomas Gonzalez <tomas@rtomasgonzalez.com>
2020-07-04 04:00:10 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -6,16 +6,16 @@ def game_hash
     home: {
       team_name: "Brooklyn Nets",
       colors: ["Black", "White"],
-      players: [
+      players: {
         "Alan Anderson" => {
-        number: 0,
-        shoe: 16,
-        points: 22,
-        rebounds: 12,
-        assists: 12,
-        steals: 3,
-        blocks: 1,
-        slam_dunks: 1
+        :number => 0,
+        :shoe => 16,
+        :points => 22,
+        :rebounds => 12,
+        :assists => 12,
+        :steals => 3,
+        :blocks => 1,
+        :slam_dunks => 1
       },
       "Reggie Evans" => {
       number: 30,
@@ -57,7 +57,7 @@ def game_hash
       blocks: 11,
       slam_dunks: 1
     }
-  ]
+  }
 },
 
   away: {
